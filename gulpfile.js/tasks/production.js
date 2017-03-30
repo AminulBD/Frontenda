fd72186073cf6e44e8dfc0fc8fa1d3a4f2586a84
Aminul Islam <aminmedia@gmail.com>
2017-03-30 13:34:53 +0600

    Fix the useref task to replace production scripts

--- gulpfile.js/tasks/production.js
+++ gulpfile.js/tasks/production.js
@@ -6,7 +6,7 @@ var getEnabledTasks = require('../lib/getEnabledTasks')
 var productionTask = function(cb) {
 	global.production = true
 	var tasks = getEnabledTasks('production')
-	gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'media', cb)
+	gulpSequence('clean', tasks.assetTasks, 'scripts', tasks.codeTasks, 'server', cb)
 }
 
 gulp.task('production', productionTask)
